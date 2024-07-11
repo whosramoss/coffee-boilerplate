5061f4ade5f71eb0be1dff61a0533d81fc806ee9
Gabriel Ramos <whosramoss@gmail.com>
2024-07-11 10:06:00 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const staticbuild = {
   distDir: 'build',
   output: 'export',
+  trailingSlash: true,
+}
+
+const nextConfig = {
+  // Comment the line to run locally or uncomment to create a static build of the project
+  ...staticbuild,
   images: {
     domains: [
       "lh3.googleusercontent.com",
